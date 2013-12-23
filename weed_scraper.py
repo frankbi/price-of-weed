fdb518d144af8f68224c5a30bbfe401a4be4dab5
root <root@ip-172-31-3-185.us-west-2.compute.internal>
2013-12-23 23:24:27 +0000

    moving files to another instance

---
+++ weed_scraper.py
@@ -0,0 +1,39 @@
+#/usr/bin/env python2.5
+
+from bs4 import BeautifulSoup
+import requests
+import re
+import csv
+import time
+
+c = csv.writer(open('weed_prices/data/weedprices' + time.strftime("%d%m%Y") + '.csv', 'wb'), quoting = csv.QUOTE_ALL)
+c.writerow(['State','HighQ','HighQN','MedQ','MedQN','LowQ','LowQN'])
+base_url = 'http://www.priceofweed.com/prices/United-States/'
+
+
+def scrape(state):
+
+	r = requests.post(base_url + state + '.html')
+	soup = BeautifulSoup(r.text)
+
+	title = soup.findAll("title")
+	state = re.sub(r' Weed Prices - PriceOfWeed.com', '', title[0].get_text())
+
+	content = soup.findAll("table", {"class":"avg_box"})
+	cell = content[0].findAll("td")
+
+	d = {
+		'state' : state,
+		'highq' : cell[4].get_text(),
+		'highq_n' : cell[5].get_text(),
+		'mediumq' : cell[7].get_text(),
+		'mediumq_n' : cell[8].get_text(),
+		'lowq' : cell[10].get_text(),
+		'lowq_n' : cell[11].get_text()
+	}
+	c.writerow([d['state'],d['highq'],d['highq_n'],d['mediumq'],d['mediumq_n'],d['lowq'],d['lowq_n']])
+
+with open('weed_prices/states.csv', 'r') as states:
+	states_read = csv.reader(states, delimiter=',', quotechar='"')
+	for row in states_read:
+		scrape(row[0])
