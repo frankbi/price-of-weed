24aa784e8c02a88f742958cb001a6ed13f6a8bff
Frank Bi <fbi@newshour.org>
2014-08-05 22:17:03 -0400

    prices for 2014-08-04

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# get today's year, month and day for commit message
+yearmonthday=$(date -d "yesterday 13:00 " '+%Y-%m-%d')
+
+# pull before running script
+git pull
+
+python weed_scraper.python
+
+git add -A
+
+git commit -a -m "prices for $yearmonthday"
+
+git push
